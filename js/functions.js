86e0e596c3412116d56c62caed53e3e851ae5839
sofiasoldano <30936938+sofiasoldano@users.noreply.github.com>
2018-05-31 15:40:47 -0300

    Init

---
+++ js/functions.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+    
+    /* Sliders */ 
+    $('.recetas-slider').flexslider({
+        animation: "slide",
+        animationLoop: false,
+        slideshow: false,
+        animationLoop: true,
+        directionNav: false,
+        itemWidth: 314,
+        itemMargin: 15,
+        minItems: 1.1
+        
+    });
+    $('.productos-slider').flexslider({
+        animation: "slide",
+        animationLoop: false,
+        slideshow: false,
+        animationLoop: true,
+        directionNav: false,
+        minItems: 1
+    });  
+    $('.desayunos-slider').flexslider({
+        animation: "slide",
+        animationLoop: false,
+        slideshow: false,
+        animationLoop: true,
+        directionNav: false,
+        minItems: 1
+    });
+
+    // Change color on scroll
+    $(document).scroll(function() {
+        headerActive();
+    });
+    
+    // Open mobile menu
+	$(".menu-icon").click(function() {
+        if($(".menu-icon").hasClass("menu-close")){
+            $(".menu").removeClass("show");   
+            $(".menu-icon").removeClass("menu-close");   
+            headerActive();
+        }
+        else{
+            $(".menu").addClass("show"); 
+            $(".menu-icon").addClass("menu-close");
+            $("header").addClass("active");
+            $("header .logo-default").addClass("hidden");
+            $("header .logo-active").removeClass("hidden");
+        }
+    });
+    
+});
+
+// Fixed Menu - change color
+function headerActive(){
+    var top_of_window = $(window).scrollTop();
+    if( top_of_window != 0){
+        $("header").addClass("active");
+        $("header .logo-default").addClass("hidden");
+        $("header .logo-active").removeClass("hidden");
+    }
+    else if(!$(".menu").hasClass("show")){
+         $("header").removeClass("active");
+        $("header .logo-default").removeClass("hidden");
+        $("header .logo-active").addClass("hidden");
+    }
+}
+
+//Submenu
+function submenu(id, number){
+    $(".submenu li").removeClass("active");
+    $(".submenu li:nth-of-type(" + number +")").addClass("active");
+    $(".menu .content .inner-content").addClass("hidden");
+    $(id).removeClass("hidden");
+}
